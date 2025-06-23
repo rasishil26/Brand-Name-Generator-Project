045764222640b1252a5593f20fa7312657b76f09
Rasi Shil <rasishil26@gmail.com>
2025-06-23 14:48:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+# Brand Name Generator
 
+print("Welcome to the Brand Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+brand_name = city + " " + pet
+
+print("Your brand name could be: " + brand_name)
